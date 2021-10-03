653f5147b17567a3a6d9ae497d4117fe66e189a8
T0w3r <torres.alejo2001@gmail.com>
2021-10-03 20:55:02 -0300

    connected to mongo database

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const morgan = require('morgan');
 const app = express();
-
+const {mongoose} = require('./database')
 //settings
 app.set("port", process.env.PORT || 3001);
 //Middlewares
