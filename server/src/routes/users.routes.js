3f86565e971e7ece8d693ae0df6683f3af7a213a
T0w3r <torres.alejo2001@gmail.com>
2021-10-04 15:59:09 -0300

    set route for post method

--- server/src/routes/users.routes.js
+++ server/src/routes/users.routes.js
@@ -2,15 +2,13 @@ const  express  = require("express");
 const router = express.Router();
 const User = require('../models/user')
 
-router.get('/', (req,res)=>{
-    User.find(function (err,users) {
-        console.log(users)
-    })
-
-    res.json({
-        status:'it Works!'
-    })
+router.get('/', async (req,res)=>{
+    const users = await User.find()
+    res.json(users)
 })
 
+router.post('/',async (req,res) =>{
+    console.log(req.body)
+})
 
 module.exports = router
