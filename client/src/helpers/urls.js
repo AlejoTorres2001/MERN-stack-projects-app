f664e9e283e7c52469aabdbb611d8cd0ab7303ee
T0w3r <torres.alejo2001@gmail.com>
2021-10-09 00:16:30 -0300

    fixed delete

--- client/src/helpers/urls.js
+++ client/src/helpers/urls.js
@@ -2,7 +2,7 @@ export const PROXY = "http://localhost:8080/";
 export const USERS = "http://localhost:3001/api/users";
 export const LOGIN = "/login";
 export const UPDATE = (id) => (id ? `/update/${id}` : "/update/id");
-export const DELETE = (id) => (id ? `/delete/${id}` : "/update/id");
+export const DELETE = (id) => (id ? `/delete/${id}` : "/delete/id");
 export const setOptions = (method, data = {}) => {
   return {
     method: method,
