3f53caf25d939b44d05ad2feab741e97293d2c46
T0w3r <torres.alejo2001@gmail.com>
2021-10-08 23:43:14 -0300

    added messages to responses helper

---
+++ server/src/helpers/responses.js
@@ -0,0 +1,17 @@
+module.exports= {
+    codes:{
+        success: 0,
+        error: 1,
+    },
+    message:{
+        emailError:"This email is already in use!",
+        usernameError:"This user name is already in use!",
+        userCreated:"Username Successfully Created!",
+        userUpdated:"Account successfuly updated!",
+        userDeleted:"Account successfuly deleted!",
+        loginError:"Incorrect username or password",
+        error:"Ups! there has been an error",
+
+    }
+    
+}
--- server/src/routes/users.routes.js
+++ server/src/routes/users.routes.js
@@ -1,9 +1,20 @@
 const express = require("express");
-const responseCodes = require("../helpers/responseCodes");
+const responses = require("../helpers/responses");
 const roles = require("../helpers/roles");
 const user = require("../models/user");
 const router = express.Router();
 const User = require("../models/user");
+
+const { error, success } = responses.codes;
+const {
+  emailError,
+  usernameError,
+  userCreated,
+  userUpdated,
+  loginError,
+  error: genericError,
+  userDeleted,
+} = responses.message;
 
 router.get("/", async (req, res) => {
   const users = await User.find();
@@ -19,9 +30,9 @@ router.post("/", async (req, res) => {
     (await User.find({ email: email })).length >= 1 ? false : true;
   const validateNewUser = (validEmail, validName) => {
     if (!validEmail)
-      responses.push({ code: responseCodes.error, message: "This email is already in use!" });
+      responses.push({ code: responses.error, message: emailError });
     if (!validName)
-      responses.push({ code: responseCodes.error, message: "This user name is already in use!" });
+      responses.push({ code: responses.error, message: usernameError });
     return validName && validEmail;
   };
   if (validateNewUser(validName, validEmail)) {
@@ -30,56 +41,50 @@ router.post("/", async (req, res) => {
       email,
       password,
       role: roles.regular,
-      profilePic:'',
+      profilePic: "",
     });
     try {
       await newUser.save();
-      responses.push({ code: responseCodes.success, message: "Username Successfully Created!" });
-      
+      responses.push({ code: responses.success, message: userCreated });
     } catch (error) {
-      responses.push({ code: responseCodes.error, message: "Ups! there has been an error" });
+      responses.push({ code: responses.error, message: genericError });
     }
   }
 
   res.json({ responses });
 });
-router.post('/login',async (req,res) =>{
-  const errors = []
-  const {name,password} = req.body
-  const getUser= async (name,password)=>{
-    const user=await User.find({name,password})
-    return user
+router.post("/login", async (req, res) => {
+  const errors = [];
+  const { name, password } = req.body;
+  const getUser = async (name, password) => {
+    const user = await User.find({ name, password });
+    return user;
+  };
+  const isValid = (user) => user?.length > 0;
+  const user = await getUser(name, password);
+  if (isValid(user)) {
+    res.json(user);
+  } else {
+    errors.push(loginError);
+    res.json({ errors });
   }
-  const isValid=(user)=> user?.length > 0
-  const user = await getUser(name,password)
-  if(isValid(user)) 
-  {res.json(user)}
-  else{
-    errors.push("Incorrect username or password")
-      res.json({errors})
+});
+router.put("/update/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    await user.findOneAndUpdate({ _id: id }, { ...req.body }, { new: true });
+    res.json({ code: responses.codes.success, message: userUpdated });
+  } catch (error) {
+    res.json({ code: responses.codes.error, message: genericError });
   }
-})
-router.put('/update/:id',async (req,res)=>{
-    const {id} = req.params
-    try {
-     await user.findOneAndUpdate({_id:id},{...req.body},{new:true})
-     res.json({code:responseCodes.success,message:"Account successfuly updated!"})
-      
-    } catch (error) {
-      res.json({code:responseCodes.error,message:"Ups! there has been an error"})
-    }
-
-})
-router.delete('/delete/:id',async (req,res) =>{
-    const {id}=req.params
-    try {
-        await user.findOneAndRemove({_id:id})
-        res.json({code:responseCodes.success,message:"Account successfuly deleted!"})
-
-    } catch (error) {
-      res.json({code:responseCodes.error,message:"Ups! there has been an error"})
-      
-      
-    }
-})
+});
+router.delete("/delete/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    await user.findOneAndRemove({ _id: id });
+    res.json({ code: responses.codes.success, message: userDeleted });
+  } catch (error) {
+    res.json({ code: responses.codes.error, message: genericError });
+  }
+});
 module.exports = router;
