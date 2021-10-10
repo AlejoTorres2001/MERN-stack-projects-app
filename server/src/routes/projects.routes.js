c293e2d1b48d2fff974bae6461cd2365b9dbe275
T0w3r <torres.alejo2001@gmail.com>
2021-10-10 18:46:43 -0300

    proyects route

--- server/src/routes/projects.routes.js
+++ server/src/routes/projects.routes.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const { codes, message } = require("../helpers/responses");
+const project = require("../models/project");
 const router = express.Router();
 
 
-router.get('/',(req,res) =>{
-
-    res.json("Working!")
+router.post('/',async (req,res) =>{
+    const {name} = req.body
+    try {
+        console.log(name)
+        const  projects = await project.find({owner:name})
+        console.log(projects)
+        res.json(projects)
+    } catch (error) {
+        res.json({code:codes.error,message:message.error})
+        
+    }
+    
 
 
 
