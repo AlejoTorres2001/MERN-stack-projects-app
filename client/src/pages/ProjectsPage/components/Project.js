0757824b513a88a2c6fc1554a10674892b429a4b
Alejo <80418452+AlejoTorres2001@users.noreply.github.com>
2021-10-16 18:15:58 -0300

    basic model

--- client/src/pages/ProjectsPage/components/Project.js
+++ client/src/pages/ProjectsPage/components/Project.js
@@ -3,7 +3,11 @@ import React from 'react'
 const Project = ({project}) => {
     return (
         <div>
-            {project.name}
+            <h1>{project.name}</h1>
+            <h2>{project.description}</h2>
+            {project.coWorkers.map((cw)=><h3>cw</h3>)}
+
+
         </div>
     )
 }
